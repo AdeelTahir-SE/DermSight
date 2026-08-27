05572341afd1838121c30eee6a0301237f42ea45
Adeel <adeeltahir6a@gmail.com>
2026-08-27 13:22:09 +0500

    chore: add initial Tailwind CSS configuration file

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,9 @@
+import "../../global.css";
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
-
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
-
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
